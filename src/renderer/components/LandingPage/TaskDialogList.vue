b7aab925bc21dd52c7e9da295c7940c9c45da78c
anipro <yuki.matsuyama0123@gmail.com>
2018-01-28 22:27:59 +0900

    update github

---
+++ src/renderer/components/LandingPage/TaskDialogList.vue
@@ -0,0 +1,25 @@
+<template>
+    <el-collapse v-model="activeNames" @change="handleChange">
+      <el-collapse-item title="作業済み" name="3">
+          <li>作業済みItem1</li>
+          <li>作業済みItem2</li>
+          <li>作業済みItem3</li>
+          <li>作業済みItem4</li>
+      </el-collapse-item>
+    </el-collapse>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        activeNames: ['1'],
+        checked: false
+      }
+    },
+    methods: {
+      handleChange (val) {
+        console.log(val)
+      }
+    }
+  }
+</script>
